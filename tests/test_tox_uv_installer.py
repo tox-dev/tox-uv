f7e2af9f06b4899a724d9d34508ecf4ba7fb41d2
Daniel Porter <dpreid@gmail.com>
2024-03-14 17:59:26 +0000

    Test all valid and an invalid uv_resolution strategy

--- tests/test_tox_uv_installer.py
+++ tests/test_tox_uv_installer.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import sys
 from typing import TYPE_CHECKING
 
+import pytest
+
 if TYPE_CHECKING:
-    import pytest
     from tox.pytest import ToxProjectCreator
 
 
@@ -62,21 +63,24 @@ def test_uv_install_without_pre_custom_install_cmd(tox_project: ToxProjectCreato
     result.assert_success()
 
 
-def test_uv_install_with_resolution_strategy(tox_project: ToxProjectCreator) -> None:
-    project = tox_project({
-        "tox.ini": """
-    [testenv]
-    deps = tomli>=2.0.1
-    package = skip
-    uv_resolution = lowest
-    """
-    })
+@pytest.mark.parametrize("strategy", ["highest", "lowest", "lowest-direct"])
+def test_uv_install_with_resolution_strategy(tox_project: ToxProjectCreator, strategy: str) -> None:
+    project = tox_project({"tox.ini": f"[testenv]\ndeps = tomli>=2.0.1\npackage = skip\nuv_resolution = {strategy}"})
     execute_calls = project.patch_execute(lambda r: 0 if "install" in r.run_id else None)
 
     result = project.run("-vv")
     result.assert_success()
 
-    assert execute_calls.call_args[0][3].cmd[2:] == ["install", "--resolution", "lowest", "tomli>=2.0.1", "-v"]
+    assert execute_calls.call_args[0][3].cmd[2:] == ["install", "--resolution", strategy, "tomli>=2.0.1", "-v"]
+
+
+def test_uv_install_with_invalid_resolution_strategy(tox_project: ToxProjectCreator) -> None:
+    project = tox_project({"tox.ini": "[testenv]\ndeps = tomli>=2.0.1\npackage = skip\nuv_resolution = invalid"})
+
+    result = project.run("-vv")
+    result.assert_failed(code=1)
+
+    assert "Invalid value for uv_resolution: 'invalid'." in result.out
 
 
 def test_uv_install_with_resolution_strategy_custom_install_cmd(tox_project: ToxProjectCreator) -> None:
